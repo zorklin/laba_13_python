188ce79925a494a0ef654b016e2a2caa9c10d262
PavloMesha <147807027+PavloMesha@users.noreply.github.com>
2024-10-31 18:47:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -25,6 +25,32 @@ def convert_csv_to_json(csv_file, json_file):
     except Exception as err:
         print(f"err: {err}")
 
+# Функція читання файлу json та перезапису його у csv з додаванням строки - Павло Меша
+def convert_json_csv(data, f_json, f_csv):
+    jsonData = json.dumps(dictionary_data)
+    try:
+        with open(f_json, "rt") as file:
+            data = json.loads(jsonData)
+    except IOError as err:
+        print(f"err with file: {err}")
+    except Exception as err:
+        print(f"err: {err}")
+    write_to_csv(data, f_csv)
+    print("Add ")
+    Name = input("Name:")
+    Age = input("Age:")
+    City = input("City:")
+    try:
+        with open(f_csv,"a") as csvfile:
+            writer = csv.writer(csvfile, delimiter = ",")
+            writer.writerow((Name, Age, City))
+            csvfile.close
+    except IOError as err:
+        print(f"err with file: {err}")
+    except Exception as err:
+        print(f"err: {err}")
+
+
 dictionary_data = [
     {"Name": "Ivan", "Age": 19, "City": "Sumy"},
     {"Name": "Dima", "Age": 18, "City": "Lutsk"},
@@ -36,3 +62,4 @@ json_name = 'data.json'
 
 write_to_csv(dictionary_data, csv_name)
 convert_csv_to_json(csv_name, json_name)
+convert_json_csv(dictionary_data ,json_name,csv_name)
